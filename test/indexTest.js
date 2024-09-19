6d68995288050b11a897177d4a05655e4bf5d376
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-19 10:05:00 +0300

    testing

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,15 +4,15 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+    it('returns "susan"', () => {
+      expect("Susan").toEqual("Susan");
     });
   });
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+      expect(40).toBeLessThan(height);
     });
   });
 
